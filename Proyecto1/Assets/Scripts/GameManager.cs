cdd9b9607ba795acdb4a3f788b038bbbe2f96f28
Warcryer <stryder04.08@gmail.com>
2018-07-06 01:23:45 +0200

    Primer commit
    Usando GitHub en Unity

---
+++ Proyecto1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    #region Public Variables
+    [Header("\t--Public Variables--")]
+
+    [Header("Time Variables")]
+    public Text timeText;
+
+    [Header("Menu Variables")]
+    public GameObject menuPanel;
+
+    [Header("Victory Panel Variables")]
+    public GameObject victoryPanel;
+
+    [Header("Defeat Panel Variables")]
+    public GameObject defeatPanel;
+    #endregion
+
+    //Time Variables
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    //Game Variables
+    private static GameManager _instance;
+    private int gameTime;
+    private bool gameFinished = false;
+    private bool isPlaying = false;
+    private bool playerIsDead = false;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!gameFinished)
+        {
+            isPlaying = true;
+        }
+        else
+        {
+            isPlaying = false;
+            if (!playerIsDead)
+            {
+                ShowVictoryScreen();
+            }
+            else
+            {
+                ShowDefeatScreen();
+            }
+        }
+
+        if (isPlaying)
+        {
+            DisplayTime();
+            LockCursor();
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false; //si le damos al botón de Quit en Unity, parará de jugar
+#endif
+            }
+        }
+    }
+
+    void DisplayTime()
+    {
+        gameTime = (int)Time.deltaTime;
+        hours = ((int)Time.timeSinceLevelLoad - gameTime) / 3600;
+        minutes = Mathf.Abs(((int)Time.timeSinceLevelLoad - gameTime) / 60);
+        seconds = ((int)Time.timeSinceLevelLoad - gameTime) % 60;
+
+        timeText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+
+    void LockCursor()
+    {
+        if (isPlaying)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+            if (Input.GetAxis("Cancel") > 0)
+                Cursor.lockState = CursorLockMode.None;
+
+            if (Input.GetMouseButtonDown(0))
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    
+    public void GameOver()
+    {
+        playerIsDead = true;
+        gameFinished = true;
+    }
+
+    #region Game States
+    public void ShowVictoryScreen()
+    {
+
+    }
+
+    public void ShowDefeatScreen()
+    {
+        Debug.Log("Dead");
+    }
+    #endregion
+}
---
+++ Proyecto1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+#region Public Variables
+    [Header("\t--Public Variables--")]
+    public float movementSpeed = 100;
+    public float rotationSpeed = 50;
+
+
+    #endregion
+    //Player Movement
+    private CharacterController characterController;
+    private float x_input;
+    private float z_input;
+    private Vector3 movement;
+    private CollisionFlags colisionesDelPlayer = CollisionFlags.None;
+
+
+    void Start ()
+    {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+
+	void Update ()
+    {
+        x_input = Input.GetAxis("Horizontal") * movementSpeed;
+        z_input = Input.GetAxis("Vertical") * movementSpeed;
+
+        //this.transform.Rotate(0, z_input, 0);
+        //this.transform.Translate(z_input, 0, x_input);
+
+        movement = new Vector3(x_input, 0f, z_input);
+
+        colisionesDelPlayer = characterController.collisionFlags;
+
+        characterController.Move(movement * Time.deltaTime);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (colisionesDelPlayer == CollisionFlags.Below)
+            return;
+
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        if (body == null || body.isKinematic)
+            return;
+
+        //body.AddForceAtPosition(-hit.normal * weight, hit.point);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+}
